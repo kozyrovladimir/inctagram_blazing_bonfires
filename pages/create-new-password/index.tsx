0e1094b11b9028761192232520378c9675a66457
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-07-26 19:43:44 +0300

    feat: add basic layout

---
+++ pages/create-new-password/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { CreateNewPassword } from '@/pages/auth-register'
+
+const CreateNewPasswordPage = () => {
+  return <CreateNewPassword/>
+}
+
+export default CreateNewPasswordPage;
---
+++ src/pages/auth-register/index.ts
@@ -0,0 +1,7 @@
+export { SignUp } from './ui/SignUp/SignUp'
+export { SentEmail } from './ui/SentEmail/SentEmail'
+export { MergeAccounts } from './ui/MergeAccounts/MergeAccounts'
+export { ExpiredVerificationLink } from './ui/ExpiredVerificationLink/ExpiredVerificationLink'
+export { InvalidVerificationLink } from './ui/InvalidVerificationLink/InvalidVerificationLink'
+export { ConfirmedEmail } from './ui/ConfirmedEmail/ConfirmedEmail'
+export { CreateNewPassword } from './ui/CreateNewPassword/CreateNewPassword'
