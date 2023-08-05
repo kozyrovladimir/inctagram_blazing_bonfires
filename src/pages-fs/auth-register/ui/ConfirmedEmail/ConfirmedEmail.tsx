97cf08d2073fbeb86f04116b6d1e793168143c33
Aliaksandr Dzivin <aliaksandr.dzivin@gmail.com>
2023-08-05 15:26:59 +0300

    fix: return mergeAcc, ExpiredVer, ConfirmEmail

--- src/pages-fs/auth-register/ui/ConfirmedEmail/ConfirmedEmail.tsx
+++ src/pages-fs/auth-register/ui/ConfirmedEmail/ConfirmedEmail.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 
+import Image from 'next/image'
+
+import styles from './ConfirmedEmail.module.scss'
+
+import broConfirmImage from '@/public/login/broCongratulations.svg'
+import { Button, ButtonTheme } from '@/shared/ui/Button/Button'
+
 export const ConfirmedEmail = () => {
-  return <div>ConfirmedEmail</div>
+  return (
+    <div className={styles.conirmedContainer}>
+      <h3>Congratulations!</h3>
+      <p>Your email has been confirmed</p>
+      <Button theme={ButtonTheme.FILLED}>Sign In</Button>
+      <Image src={broConfirmImage} alt={'women login accaunt in her phone'} />
+    </div>
+  )
 }
