6067bc8be10e8df25994678659fd6a845a6dfa17
Aliaksandr Dzivin <aliaksandr.dzivin@gmail.com>
2023-08-07 00:00:48 +0300

    feat: coverage almost all components unit tests

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import '@testing-library/jest-dom'
+import { Button, ButtonSize, ButtonTheme } from './Button'
+
+describe('Button component', () => {
+  it('render Button', () => {
+    render(<Button />)
+
+    expect(screen.getByRole('button')).toBeInTheDocument()
+  })
+
+  it('button clicked', async () => {
+    const handleClick = jest.fn()
+
+    render(<Button onClick={handleClick} />)
+    const button = screen.getByRole('button')
+
+    await userEvent.click(button)
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+  it('button have classes', () => {
+    render(<Button theme={ButtonTheme.FILLED} size={ButtonSize.SMALL} />)
+    const button = screen.getByRole('button')
+
+    expect(button).toHaveClass('Button')
+    expect(button).toHaveClass('filled')
+    expect(button).toHaveClass('small')
+  })
+})
