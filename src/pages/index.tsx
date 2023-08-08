5597f20f87351815b0ce8a8c59cc097ad83f39cf
Aliaksandr Dzivin <aliaksandr.dzivin@gmail.com>
2023-08-08 20:03:44 +0300

    feat: example react-datapicker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { Logout } from '@/features/logout/ui/Logout/Logout'
 import { getLayout } from '@/shared/layout/MainLayout/MainLayout'
+import { Example } from '@/widgets/LangSwitcher/ui/Calendar/Calendar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ function Home() {
   return (
     <>
       <main>
+        <Example />
         <ul>
           <li>
             <Link href="/sign-in">sign-in</Link>
