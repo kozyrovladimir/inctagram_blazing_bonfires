ff6b372485c4a42ceec9c23e03038a6bfe6edef5
EkaterinaShumskaya <katya_vodich@mail.ru>
2023-08-10 14:54:47 +0300

    feat: add profile page setting

--- src/pages/profile/general-information/index.tsx
+++ src/pages/profile/general-information/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 import { ProfileSetting } from '@/features/profile-setting/ProfileSetting'
-import styles from '@/features/profile-setting/ProfileSetting.module.scss'
 import { settingLayout } from '@/shared/layout/ProfileLayout/SettingLayout'
-import { Button, ButtonSize } from '@/shared/ui/Button/Button'
 
 function GeneralInformation() {
   return (
