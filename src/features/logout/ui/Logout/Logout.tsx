f6075941b97c83639e3af9068721f4a0e9f01763
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-13 16:13:21 +0300

    fix: fix Logout imports

--- src/features/logout/ui/Logout/Logout.tsx
+++ src/features/logout/ui/Logout/Logout.tsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 
 import Image from 'next/image'
 
-import logoutImg from '../../../../public/logout/logout.svg'
-import { useLogoutMutation } from '../../../../shared/api/auth.api'
-import { ModalWindow } from '../../../../shared/modalWindow/ModalWindow'
-import { Button, ButtonSize, ButtonTheme } from '../../../../shared/ui/Button/Button'
+import logoutImg from '../../../../../public/assets/icons/logout/logout.svg'
 
 import style from './Logout.module.scss'
+
+import { useLogoutMutation } from '@/shared/api/auth.api'
+import { Button } from '@/shared/ui/Button/Button'
+import { Modal } from '@/shared/ui/Modal/Modal'
 
 export const Logout = () => {
   const [logout, { isLoading }] = useLogoutMutation()
@@ -29,7 +30,7 @@ export const Logout = () => {
         <span className={style.description}>Logout</span>
       </Button>
       {isLogout && (
-        <ModalWindow
+        <Modal
           title={'Log Out'}
           extraButton={'Yes'}
           mainButton={'No'}
@@ -38,7 +39,7 @@ export const Logout = () => {
         >
           Are you really want to log out of your account
           <span className={style.userName}> “Epam@epam.com”</span> ?
-        </ModalWindow>
+        </Modal>
       )}
     </>
   )
