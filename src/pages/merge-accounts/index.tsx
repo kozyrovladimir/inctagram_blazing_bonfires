c56345094c2ce3f507bb95ede9a748cfb6f3c88e
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-13 16:51:20 +0300

    refactor: remove unnecessary component

--- src/pages/merge-accounts/index.tsx
+++ src/pages/merge-accounts/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
-import { MergeAccounts } from '@/pages-fs/auth-register'
+import Image from 'next/image'
+
+import broImage from '../../../public/assets/icons/login/bro.svg'
+
+import styles from './MergeAccounts.module.scss'
+
 import { getLayout } from '@/shared/layout/MainLayout/MainLayout'
+import { Button, ButtonTheme } from '@/shared/ui/Button/Button'
 
 const MergeAccountsPage = () => {
-  return <MergeAccounts />
+  return (
+    <div className={styles.mergeAccContainer}>
+      <h3>Merge of Accounts</h3>
+      <p>
+        The user with email <b>Epam@epam.com</b> is already in the system. Could we merge this
+        accounts?
+      </p>
+      <Button theme={ButtonTheme.CLEAR}>Yes, merge</Button>
+      <Button theme={ButtonTheme.CLEAR}>No</Button>
+      <Image src={broImage} alt={'women login accaunt in her phone'} />
+    </div>
+  )
 }
 
 MergeAccountsPage.getLayout = getLayout
