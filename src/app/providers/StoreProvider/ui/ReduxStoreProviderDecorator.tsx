2893d7405208ff38de30bad46398d8b52cefb297
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-13 17:23:13 +0300

    refactor: replace decorator

--- src/app/providers/StoreProvider/ui/ReduxStoreProviderDecorator.tsx
+++ src/app/providers/StoreProvider/ui/ReduxStoreProviderDecorator.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Provider } from 'react-redux'
 
-import { store } from '../../../../../shared/store'
+import { store } from '../model/store'
 
 export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
   return <Provider store={store}>{storyFn()}</Provider>
--- src/features/auth-register/ui/SignUpForm/decorators/ReduxStoreProviderDecorator.tsx
+++ src/features/auth-register/ui/SignUpForm/decorators/ReduxStoreProviderDecorator.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Provider } from 'react-redux'
 
-import { store } from './../../../../../shared/store'
+import { store } from '@/app/providers/StoreProvider/model/store'
 
 export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
   return <Provider store={store}>{storyFn()}</Provider>
