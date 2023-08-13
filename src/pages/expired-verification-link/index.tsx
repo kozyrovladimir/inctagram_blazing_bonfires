05fcafb7a81a23de4ee723836f62da932ee2889f
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-13 16:46:57 +0300

    refactor: remove unnecessary component

--- src/pages/expired-verification-link/index.tsx
+++ src/pages/expired-verification-link/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-import { ExpiredVerificationLink } from '@/pages-fs/auth-register'
+import Image from 'next/image'
+
+import broResend from '../../../public/assets/icons/login/broResend.svg'
+import { Button, ButtonSize, ButtonTheme } from '../../shared/ui/Button/Button'
+
+import styles from './ExpiredVerificationLink.module.scss'
+
 import { getLayout } from '@/shared/layout/MainLayout/MainLayout'
 
 const ExpiredVerificationLinkPage = () => {
-  return <ExpiredVerificationLink />
+  return (
+    <div className={styles.expiredContainer}>
+      <h3>Email verification link expired</h3>
+      <p>Looks like the verification link has expired. Not to worry, we can send the link again</p>
+      <Button theme={ButtonTheme.FILLED} size={ButtonSize.LARGE}>
+        Resend verification link
+      </Button>
+      <Image src={broResend} alt={'men wait and look on clock'} />
+    </div>
+  )
 }
 
 ExpiredVerificationLinkPage.getLayout = getLayout
