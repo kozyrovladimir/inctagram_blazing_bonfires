f0925e62297b930a50ae6178b87fad45a93072f5
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-13 16:31:00 +0300

    refactor: remove unnecessary component

--- src/pages/auth/registration-confirmation/index.tsx
+++ src/pages/auth/registration-confirmation/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { RegistrationConfirmation } from '@/pages-fs/auth-register/ui/auth/RedistrationConfirmation/RegistrationConfirmation'
+import Link from 'next/link'
+
+import { useVerifyEmailMutation } from '@/shared/api/auth.api'
 import { getLayout } from '@/shared/layout/MainLayout/MainLayout'
+import { Button } from '@/shared/ui/Button/Button'
 
-const RegistrationConfirmationPage = () => {
-  return <RegistrationConfirmation />
+const RegistrationConfirmation = () => {
+  const [code, setCode] = useState('')
+  const [verifyEmail] = useVerifyEmailMutation()
+
+  useEffect(() => {
+    const urlCode = window.location.search.split('=')[1]
+
+    setCode(urlCode)
+  }, [])
+
+  useEffect(() => {
+    if (code) {
+      verifyEmail(code)
+    }
+  }, [code])
+
+  return (
+    <div>
+      <p>Congratulations! Your email has been confirmed</p>
+      <Link href={'/login'}>
+        <Button>Sign in</Button>
+      </Link>
+    </div>
+  )
 }
 
-RegistrationConfirmationPage.getLayout = getLayout
-export default RegistrationConfirmationPage
+RegistrationConfirmation.getLayout = getLayout
+export default RegistrationConfirmation
