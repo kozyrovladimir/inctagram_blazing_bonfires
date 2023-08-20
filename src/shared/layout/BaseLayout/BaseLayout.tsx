1b0f6057233cc1bc25eda8b61e4bf32184d8e2ae
Roman <Alimov.ramon@gmail.com>
2023-08-20 19:09:50 +0300

    feat: created my profile page

--- src/shared/layout/BaseLayout/BaseLayout.tsx
+++ src/shared/layout/BaseLayout/BaseLayout.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren, ReactElement } from 'react'
 
 import { NextPage } from 'next'
 
-import { Header } from '@/components/Header/Header'
+import { Header } from '../../../components/Header/Header'
 
 const BaseLayout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
