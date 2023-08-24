0f71acc1ccd298f373ee9e90a0224176df5b8156
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-24 18:33:29 +0300

    refactor: add titles to stories

--- src/features/profile-setting/ui/PhotoModal/PhotoModal.stories.tsx
+++ src/features/profile-setting/ui/PhotoModal/PhotoModal.stories.tsx
@@ -3,8 +3,8 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { PhotoModal } from './PhotoModal'
 
 const meta: Meta<typeof PhotoModal> = {
-  component: PhotoModal,
   title: 'Example/PhotoModal',
+  component: PhotoModal,
 }
 
 export default meta
--- src/pages/profile/Profile.stories.tsx
+++ src/pages/profile/Profile.stories.tsx
@@ -3,12 +3,11 @@ import type { Meta, StoryObj } from '@storybook/react'
 import Profile from './index'
 
 const meta: Meta<typeof Profile> = {
+  title: 'Pages/Profile',
   component: Profile,
 }
 
 export default meta
 type Story = StoryObj<typeof Profile>
 
-export const Primary: Story = {
-  render: () => <Profile />,
-}
+export const Primary: Story = {}
