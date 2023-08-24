194af6bc431321922d032c99a7cc4e5c89abb356
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-24 17:26:14 +0300

    refactor: add titles, refactor stories for existing code style

--- src/widgets/LangSwitcher/ui/LanguageSelect/LanguageSelect.stories.tsx
+++ src/widgets/LangSwitcher/ui/LanguageSelect/LanguageSelect.stories.tsx
@@ -3,12 +3,12 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { LanguageSelect } from './LanguageSelect'
 
 const meta: Meta<typeof LanguageSelect> = {
+  title: 'Example/LanguageSelect',
   component: LanguageSelect,
 }
 
 export default meta
 
 type Story = StoryObj<typeof LanguageSelect>
-export const Primary: Story = {
-  render: () => <LanguageSelect />,
-}
+
+export const Primary: Story = {}
