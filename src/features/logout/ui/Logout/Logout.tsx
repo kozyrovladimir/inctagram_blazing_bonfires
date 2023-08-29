34c898366b7719eb628880dce2ed7a9167a102d5
AlekseiCherenkevich <alekseicherenkevich@gmail.com>
2023-08-29 13:38:01 +0300

    refactor: put token removing to api

--- src/features/logout/ui/Logout/Logout.tsx
+++ src/features/logout/ui/Logout/Logout.tsx
@@ -1,39 +1,55 @@
 import React, { useState } from 'react'
 
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import style from './Logout.module.scss'
 
 import { useLogoutMutation } from '@/shared/api'
+import { useUpdateTokenMutation } from '@/shared/api/auth/auth.api'
 import logoutImg from '@/shared/assets/icons/logout/logout.svg'
 import { Button } from '@/shared/ui/Button/Button'
 import { Modal } from '@/shared/ui/Modal/Modal'
 
 export const Logout = () => {
-  const [logout, { isLoading }] = useLogoutMutation()
+  const router = useRouter()
+
+  const [logout, { isLoading, isSuccess: isLoggedOut }] = useLogoutMutation()
+  const [refresh, { isSuccess: isRefreshed }] = useUpdateTokenMutation()
 
   const logoutApiHandler = () => {
     logout()
-    logoutHandler()
+      .unwrap()
+      .then(() => {
+        router.push('/sign-in')
+      })
+      .finally(() => {
+        closeModal()
+      })
   }
 
-  const [isLogout, setIsLogout] = useState(false)
-  const logoutHandler = () => {
-    setIsLogout(!isLogout)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false)
   }
 
   return (
     <>
-      <Button className={style.logoutButton} onClick={logoutHandler}>
+      <Button className={style.logoutButton} onClick={openModal}>
         <Image src={logoutImg} alt={''} />
         <span className={style.description}>Logout</span>
       </Button>
-      {isLogout && (
+      {isModalOpen && (
         <Modal
           title={'Log Out'}
           extraButton={'Yes'}
           mainButton={'No'}
-          callBackCloseWindow={logoutHandler}
+          callBackCloseWindow={closeModal}
           extraButtonCB={logoutApiHandler}
         >
           Are you really want to log out of your account
