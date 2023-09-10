118aecf175da61260fe669bdee1e92efd27ccc7d
Nataly Baranova <borashek@inbox.ru>
2023-09-10 14:54:18 +0300

    feat: fix errors in the last merge request

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import SignUpForm from '@/features/auth-register/ui/SignUpForm/SignUpForm'
-import { getLayout } from '@/widgets/layout/MainLayout/MainLayout'
+import { getLayout } from '@/shared/layouts/MainLayout/MainLayout'
 
 const SignUp = () => {
   return <SignUpForm />
