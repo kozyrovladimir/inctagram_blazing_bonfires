21c538449efdad0a7a86a313af43e840d483ce54
Nataly Baranova <borashek@inbox.ru>
2023-09-13 14:56:46 +0300

    feat: new oauth

--- src/shared/ui/Loaders/CircularLoader.tsx
+++ src/shared/ui/Loaders/CircularLoader.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { CircularProgress } from '@mui/material'
+
 import styles from '@/shared/ui/Loaders/Loader.module.scss'
 
 export const CircularLoader = () => {
   return (
     <div className={styles.loaderWrapper}>
-      <CircularLoader />
+      <CircularProgress />
     </div>
   )
 }
