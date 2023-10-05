ce0e535a6973cabb73731a2ffeb443512d119e0e
kozyrovladimir <kozyrovladimir@gmail.com>
2023-10-05 14:37:45 +0300

    feature: create new post modal

---
+++ src/features/profile-setting/ui/newPostModal/NewPostModal.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Dialog } from '@headlessui/react';
+import styles from './NewPostModal.module.scss'
+
+interface NewPostModalProps {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  title: string;
+  left?: React.ReactNode;
+  right?: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const NewPostModal: React.FC<NewPostModalProps> = (props) => {
+  return (
+    <Dialog open={props.isOpen} onClose={() => props.setIsOpen(false)} className={styles.modalWrapper}>
+      <Dialog.Panel className={styles.modal}>
+        {/*header*/}
+        <div className={styles.modalHeader}>
+          {props.left}
+          <Dialog.Title className={styles.modalTitle}>{props.title}</Dialog.Title>
+          {props.right}
+        </div>
+        {/*content*/}
+        <div className={styles.modalContent}>
+          {props.children}
+        </div>
+      </Dialog.Panel>
+    </Dialog>
+  );
+};
+
+export default NewPostModal;
