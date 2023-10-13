6f4d70f20081abe7d0c5e38bda1e5ec5bd052cbe
Roman <Alimov.ramon@gmail.com>
2023-10-13 18:20:25 +0300

    fix: fixed it for the build: forgotPassForm, PostModal/ RoundRedBtn

--- src/shared/ui/roundRedBtn/Button.stories.tsx
+++ src/shared/ui/roundRedBtn/Button.stories.tsx
@@ -1,56 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button, ButtonSize, ButtonTheme } from './roundRedBtn'
+import { RoundRedBtn } from './roundRedBtn'
 
-const meta: Meta<typeof Button> = {
-  component: Button,
-  title: 'Example/Button',
-  parameters: {
-    layout: 'centered',
-  },
-  argTypes: { onClick: { action: 'clicked' } },
-  tags: ['autodocs'],
+const meta: Meta<typeof RoundRedBtn> = {
+  component: RoundRedBtn,
+  title: 'Example/RoundRedBtn',
 }
 
 export default meta
-type Story = StoryObj<typeof Button>
+type Story = StoryObj<typeof RoundRedBtn>
 
-export const FilledButton: Story = {
-  args: {
-    children: 'Click',
-    theme: ButtonTheme.FILLED,
-    size: ButtonSize.MIDDLE,
-  },
-}
-
-export const ClearButton: Story = {
-  args: {
-    children: 'Click',
-    theme: ButtonTheme.CLEAR,
-    size: ButtonSize.MIDDLE,
-  },
-}
-
-export const SmallButton: Story = {
-  args: {
-    children: 'OK',
-    theme: ButtonTheme.CLEAR,
-    size: ButtonSize.SMALL,
-  },
-}
-
-export const LargeButton: Story = {
-  args: {
-    children: 'Click',
-    theme: ButtonTheme.FILLED,
-    size: ButtonSize.LARGE,
-  },
-}
-
-export const StretchedButton = () => {
-  return (
-    <div style={{ width: '400px' }}>
-      <Button size={ButtonSize.STRETCHED}>Stretched</Button>
-    </div>
-  )
+export const RoundRedButton: Story = {
+  args: {},
 }
