df26567b3bc744de12bd71b730b4b63604c66e10
kozyrovladimir <kozyrovladimir@gmail.com>
2023-10-23 19:05:31 +0300

    refactor: some refactor

--- src/features/profile-setting/ui/newPostModal/components/CropSlider/CropSlider.tsx
+++ src/features/profile-setting/ui/newPostModal/components/CropSlider/CropSlider.tsx
@@ -17,6 +17,7 @@ import {
 import {
   Cropping
 } from "@/features/profile-setting/ui/newPostModal/steps/Cropping/Cropping";
+import styles from "./CroppingSlider.module.scss"
 
 
 const CropSlider = () => {
@@ -35,7 +36,7 @@ const CropSlider = () => {
           touchEnabled={false}
           dragEnabled={false}
         >
-          <Slider>
+          <Slider className={styles.slider}>
             {
               cropContext.photos.map((photo, index) => (
                 <Slide index={index}>
