f27fb6c4ea186ff7f3a1c66051fc662589adad47
Nataly Baranova <borashek@inbox.ru>
2023-10-30 13:35:25 -0300

    feat: add subscriptions

--- src/features/profile-setting/ui/management/Management.tsx
+++ src/features/profile-setting/ui/management/Management.tsx
@@ -7,47 +7,46 @@ import styles from './Management.module.scss'
 
 import payPal from '@/shared/assets/icons/payments/payPal.svg'
 import stripe from '@/shared/assets/icons/payments/stripe.svg'
-import { Modal } from '@/shared/ui/Modal/Modal'
-import { RoundCheckbox } from '@/shared/ui/roundCheckbox/roundCheckbox'
+import { Modal } from '@/shared/ui/modal/Modal'
+import { RoundCheckbox } from '@/shared/ui/roundCheckbox/RoundCheckbox'
+
+type AccType = {
+  id: number
+  name: string
+}
+const accTypes: AccType[] = [
+  { id: 0, name: 'personal' },
+  { id: 1, name: 'business' },
+]
+
+type PriceType = {
+  id: number
+  value: string
+}
+const prices: PriceType[] = [
+  { id: 0, value: '$10 per 1 Day' },
+  { id: 1, value: '$50 per 7 Day' },
+  { id: 2, value: '$100 per month' },
+]
+
+// helper function
+function capitalizeFirstLetter(inputString) {
+  if (inputString.length === 0) {
+    return inputString
+  }
+
+  return inputString.charAt(0).toUpperCase() + inputString.slice(1)
+}
 
 export const Management = () => {
-  const [personalChecked, setPersonalChecked] = useState(true)
-  const [businessChecked, setBusinessChecked] = useState(false)
-
-  const [tenPerOneDay, setTenPerOneDay] = useState(true)
-  const [fiftyPerSevenDays, setFiftyPerSevenDays] = useState(false)
-  const [oneHundredPerMonth, setOneHundredPerMonth] = useState(false)
-
   const [subscribed, setSubscribed] = useState(false)
   const callBackCloseWindow = () => setSubscribed(false)
 
-  const handlePersonalChange = () => {
-    setPersonalChecked(!personalChecked)
-    setBusinessChecked(false)
-  }
+  const [accType, setAccType] = useState(0)
+  const handleChangeAccType = (value: number) => setAccType(value)
 
-  const handleBusinessChange = () => {
-    setBusinessChecked(!businessChecked)
-    setPersonalChecked(false)
-  }
-
-  const handleTenPerOneDay = () => {
-    setTenPerOneDay(true)
-    setFiftyPerSevenDays(false)
-    setOneHundredPerMonth(false)
-  }
-
-  const handleFiftyPerSevenDays = () => {
-    setTenPerOneDay(false)
-    setFiftyPerSevenDays(true)
-    setOneHundredPerMonth(false)
-  }
-
-  const handleOneHundredPerMonth = () => {
-    setTenPerOneDay(false)
-    setFiftyPerSevenDays(false)
-    setOneHundredPerMonth(true)
-  }
+  const [accPrice, setAccPrice] = useState(0)
+  const handleAccPriceType = (value: number) => setAccPrice(value)
 
   const choosePaymentMethod = () => console.log('choose payment method')
 
@@ -67,38 +66,35 @@ export const Management = () => {
         <div>
           <h3 className={styles.title}>Account type:</h3>
           <div className={styles.listWrapper}>
-            <RoundCheckbox
-              checked={personalChecked}
-              onChange={handlePersonalChange}
-              label={<p className={styles.listItem}>Personal</p>}
-            />
-            <RoundCheckbox
-              checked={businessChecked}
-              onChange={handleBusinessChange}
-              label={<p className={styles.listItem}>Business</p>}
-            />
+            {accTypes.map(item => {
+              return (
+                <RoundCheckbox
+                  id={item.id}
+                  key={item.id}
+                  name={'accType'}
+                  onChangeCheckbox={() => handleChangeAccType(item.id)}
+                  label={<p className={styles.listItem}>{capitalizeFirstLetter(item.name)}</p>}
+                />
+              )
+            })}
           </div>
         </div>
-        {businessChecked && (
+        {accType === 1 && (
           <>
             <div>
               <h3 className={styles.title}>Your subscription costs:</h3>
               <div className={styles.listWrapper}>
-                <RoundCheckbox
-                  checked={tenPerOneDay}
-                  onChange={handleTenPerOneDay}
-                  label={<p className={styles.listItem}>$10 per 1 Day</p>}
-                />
-                <RoundCheckbox
-                  checked={fiftyPerSevenDays}
-                  onChange={handleFiftyPerSevenDays}
-                  label={<p className={styles.listItem}>$50 per 7 Day</p>}
-                />
-                <RoundCheckbox
-                  checked={oneHundredPerMonth}
-                  onChange={handleOneHundredPerMonth}
-                  label={<p className={styles.listItem}>$100 per month</p>}
-                />
+                {prices.map(item => {
+                  return (
+                    <RoundCheckbox
+                      id={item.id}
+                      key={item.id}
+                      name={'accPrice'}
+                      onChangeCheckbox={() => handleAccPriceType(item.id)}
+                      label={<p className={styles.listItem}>{item.value}</p>}
+                    />
+                  )
+                })}
               </div>
             </div>
             <div className={styles.footerWrapper}>
--- src/shared/ui/roundCheckbox/RoundCheckbox.tsx
+++ src/shared/ui/roundCheckbox/RoundCheckbox.tsx
@@ -1,26 +1,28 @@
 import { forwardRef, ReactNode, Ref } from 'react'
+import * as React from 'react'
 
-import inputStyles from './../Input/Input.module.scss'
-import styles from './roundCheckbox.module.scss'
+import inputStyles from './../input/Input.module.scss'
+import styles from './RoundCheckbox.module.scss'
 
 type Props = {
   ref: Ref<HTMLInputElement>
   label: ReactNode
-  value?: string
   disabled?: boolean
   error?: string
+  id: number
+  onChangeCheckbox: (value: string) => void
 }
 
 export const RoundCheckbox = forwardRef<HTMLInputElement, Props>(
-  ({ label, disabled, error, value, ...rest }: Props, ref) => {
+  ({ id, label, disabled, error, value, onChangeCheckbox, ...rest }: Props, ref) => {
     return (
       <>
         <div className={styles.checkboxWrapper}>
           <input
-            value={value}
+            value={id}
             type={'radio'}
+            onChange={() => onChangeCheckbox(value)}
             className={styles.checkbox}
-            disabled={disabled}
             {...rest}
             ref={ref}
           />
