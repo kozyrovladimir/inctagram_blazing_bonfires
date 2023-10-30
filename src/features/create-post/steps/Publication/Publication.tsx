ac3276edd7a8bb21ef273de7e3bd38afe810c6c3
EkaterinaShumskaya <katya_vodich@mail.ru>
2023-10-30 17:31:50 +0300

    fix: add publish logic

--- src/features/create-post/steps/Publication/Publication.tsx
+++ src/features/create-post/steps/Publication/Publication.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, useState } from 'react'
 
 import Image from 'next/image'
+import { toast, Toaster } from 'react-hot-toast'
 import { useWizard } from 'react-use-wizard'
 
 import style from './Publication.module.scss'
@@ -20,6 +21,7 @@ import next from '@/shared/assets/icons/filterPostPhoto/next.svg'
 import prev from '@/shared/assets/icons/filterPostPhoto/prev.svg'
 import { Button, ButtonTheme } from '@/shared/ui/button/Button'
 import { Input, InputType } from '@/shared/ui/input/Input'
+import { LinearLoader } from '@/shared/ui/loaders/LinearLoader'
 
 export const Publication = () => {
   const { isOpen, setIsOpen } = useImageCropContext()
@@ -29,10 +31,11 @@ export const Publication = () => {
   const { currentIndex, prevSlide, nextSlide } = useSlider(cropContext.photos.length)
   const { data } = useMeQuery()
   const { data: profileData } = useGetProfileQuery(data?.userId ? data?.userId.toString() : '')
-  const [uploadImage] = useUploadImageMutation()
-  const [createPost] = useCreatePostMutation()
+  const [uploadImage, { isLoading: isUploadLoading }] = useUploadImageMutation()
+  const [createPost, { isLoading: isCreatePostLoading }] = useCreatePostMutation()
 
   const avatar = profileData?.avatars[0].url
+  const isLoading = isUploadLoading || isCreatePostLoading
 
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value)
@@ -43,104 +46,122 @@ export const Publication = () => {
 
     // преобразование url всех изображений в file
     for (const photo of cropContext.photos) {
-      console.log('lll', cropContext.photos)
       const result = await fetch(photo.url)
       const blob = await result.blob()
-      const file = new File([blob], 'image', { type: 'image/jpeg' })
+      const file = new File([blob], 'image', { type: 'image/jpg' })
 
       // Добавление file в FormData
-      // formData.append('files', file)
+      formData.append('file', file)
     }
 
     uploadImage(formData)
       .unwrap()
       .then(res => {
-        console.log(res)
+        const uploadIds = res.images.map(image => image.uploadId)
+
+        const body = {
+          description: text,
+          childrenMetadata: uploadIds.map(uploadId => ({ uploadId })),
+        }
+
+        createPost(body)
+          .unwrap()
+          .then(res => {
+            toast.success('Post Created')
+            setIsOpen(!isOpen)
+          })
+          .catch(error => {
+            toast.error(error.data.messages[0].message)
+          })
       })
       .catch(error => {
-        console.log('Error uploading images:', error)
+        toast.error(error.data.messages)
       })
   }
 
   return (
-    <NewPostModal
-      isOpen={isOpen}
-      title={'Publication'}
-      setIsOpen={setIsOpen}
-      left={<Image src={backIcon} alt={''} onClick={previousStep} />}
-      right={<span onClick={handlePublish}>Publish</span>}
-    >
-      <div className={style.publishModalContent}>
-        <div className={style.sliderWrapper}>
-          <ImageFilter
-            className={style.sliderImage}
-            image={cropContext.photos[currentIndex].croppedUrl}
-            filter={cropContext.photos[currentIndex].filter}
-            onChange={(filteredImg: string) => {
-              cropContext.setFilteredUrl(filteredImg, currentIndex)
-            }}
-            preserveAspectRatio={'contain'}
-          />
-          {cropContext.photos.length > 1 && (
-            <>
-              <div className={style.sliderButtonsContainer}>
-                <Button
-                  theme={ButtonTheme.CLEAR}
-                  className={style.sliderButton}
-                  onClick={prevSlide}
-                >
-                  <Image src={prev} alt={''} />
-                </Button>
-                <Button
-                  theme={ButtonTheme.CLEAR}
-                  className={style.sliderButton}
-                  onClick={nextSlide}
-                >
-                  <Image src={next} alt={''} />
-                </Button>
+    <>
+      <Toaster position={'bottom-center'} />
+      {isLoading && <LinearLoader />}
+      <NewPostModal
+        isOpen={isOpen}
+        title={'Publication'}
+        setIsOpen={setIsOpen}
+        left={<Image src={backIcon} alt={''} onClick={previousStep} />}
+        right={<span onClick={handlePublish}>Publish</span>}
+      >
+        <div className={style.publishModalContent}>
+          <div className={style.sliderWrapper}>
+            <ImageFilter
+              className={style.sliderImage}
+              image={cropContext.photos[currentIndex].croppedUrl}
+              filter={cropContext.photos[currentIndex].filter}
+              onChange={(filteredImg: string) => {
+                cropContext.setFilteredUrl(filteredImg, currentIndex)
+              }}
+              preserveAspectRatio={'contain'}
+            />
+            {cropContext.photos.length > 1 && (
+              <>
+                <div className={style.sliderButtonsContainer}>
+                  <Button
+                    theme={ButtonTheme.CLEAR}
+                    className={style.sliderButton}
+                    onClick={prevSlide}
+                  >
+                    <Image src={prev} alt={''} />
+                  </Button>
+                  <Button
+                    theme={ButtonTheme.CLEAR}
+                    className={style.sliderButton}
+                    onClick={nextSlide}
+                  >
+                    <Image src={next} alt={''} />
+                  </Button>
+                </div>
+                <div className={style.sliderDotsBarWrapper}>
+                  <DotsBar activeIndex={currentIndex} count={cropContext.photos.length} />
+                </div>
+              </>
+            )}
+          </div>
+          <div className={style.publish}>
+            <div className={style.publishContent}>
+              <div className={style.avatarWrapper}>
+                {avatar && (
+                  <Image
+                    src={avatar}
+                    alt="userPhoto"
+                    className={style.avatar}
+                    width={45}
+                    height={45}
+                  />
+                )}
+                <div className={style.userName}>{profileData?.userName}</div>
               </div>
-              <div className={style.sliderDotsBarWrapper}>
-                <DotsBar activeIndex={currentIndex} count={cropContext.photos.length} />
-              </div>
-            </>
-          )}
-        </div>
-        <div className={style.publish}>
-          <div className={style.publishContent}>
-            <div className={style.avatarWrapper}>
-              {avatar && (
-                <Image
-                  src={avatar}
-                  alt="userPhoto"
-                  className={style.avatar}
-                  width={45}
-                  height={45}
+              <div className={style.description}>
+                <label className={style.label}>Add publication descriptions</label>
+                <textarea
+                  rows={6}
+                  cols={60}
+                  value={text}
+                  maxLength={500}
+                  onChange={handleChange}
+                  style={{ backgroundColor: 'black', width: '100%' }}
                 />
-              )}
-              <div className={style.userName}>{profileData?.userName}</div>
-            </div>
-            <div className={style.description}>
-              <label className={style.label}>Add publication descriptions</label>
-              <textarea
-                rows={6}
-                cols={60}
-                value={text}
-                maxLength={500}
-                onChange={handleChange}
-                style={{ backgroundColor: 'black', width: '100%' }}
-              />
-              <div className={style.maxLength}> {text.length}/500</div>
-              <Input
-                label={'Add location'}
-                placeholder={''}
-                type={InputType.LOCATION}
-                style={{ marginBottom: '20px' }}
-                classNameWrap={'myCustomLabel'}
-              />
+                <div className={style.maxLength}> {text.length}/500</div>
+                <Input
+                  label={'Add location'}
+                  placeholder={''}
+                  type={InputType.LOCATION}
+                  style={{ marginBottom: '20px' }}
+                  classNameWrap={'myCustomLabel'}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </NewPostModal>
+      </NewPostModal>
+    </>
   )
 }
