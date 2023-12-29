a653641d9acbebcfec6fc817f0df1c3c3a31a972
DK-AC <Denkacaj@gmail.com>
2023-12-29 16:48:42 +0300

    BB-102:fix devices name rendering refactoring: Device props

--- src/features/profile-setting/ui/device/Device.tsx
+++ src/features/profile-setting/ui/device/Device.tsx
@@ -3,38 +3,23 @@ import { useTranslation } from 'next-i18next'
 
 import styles from './Device.module.scss'
 
-import desctopImage from '@/shared/assets/icons/devices/lightIcons/desktop.svg'
+import { UserSessionsType } from '@/shared/api'
+import desktopImage from '@/shared/assets/icons/devices/lightIcons/desktop.svg'
 import mobileImage from '@/shared/assets/icons/devices/lightIcons/mobile.svg'
 import logoutImg from '@/shared/assets/icons/logout/logout.svg'
 import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/button/Button'
 import { IconDevice } from '@/shared/ui/iconDevice/IconDevice'
 
 type Props = {
-  deviceName: string
-  lastActive: string
-  browserName: string
-  deviceId: number
-  ip: string
   isCurrent: boolean
   logoutCallback?: (id: number) => void
-  osName: string
-  deviceType: string
+  sessionData: UserSessionsType
 }
-export const Device = ({
-  osName,
-  ip,
-  isCurrent,
-  browserName,
-  lastActive,
-  deviceId,
-  logoutCallback,
-  deviceName,
-  deviceType,
-}: Props) => {
-  const {
-    t,
-    i18n: { t: tRoot },
-  } = useTranslation('common', { keyPrefix: 'Auth' })
+export const Device = ({ isCurrent, logoutCallback, sessionData }: Props) => {
+  const { deviceType, deviceId, deviceName, osName, browserName, lastActive, osVersion, ip } =
+    sessionData
+
+  const { t } = useTranslation('common', { keyPrefix: 'Auth' })
 
   return (
     <>
@@ -50,9 +35,9 @@ export const Device = ({
             </>
           ) : (
             <>
-              <Image src={deviceType === 'mobile' ? mobileImage : desctopImage} alt="device" />
+              <Image src={deviceType === 'mobile' ? mobileImage : desktopImage} alt="device" />
               <div className={styles.description}>
-                <h4>{`${osName === 'Mac OS' || osName === 'iOS' ? 'Apple' : ''} ${deviceName}`}</h4>
+                <h4>{`${osName} ${osVersion} ${deviceName ? deviceName : ''}`}</h4>
                 <p>IP: {ip}</p>
                 <p>Last visit: {new Date(lastActive).toLocaleDateString()}</p>
               </div>
