77347a851fb4b7b1d147d882f3b11695ba9d9091
DK-AC <Denkacaj@gmail.com>
2024-01-12 13:09:04 +0300

    uc-14 feat: fix styles comment

--- src/entities/postModal/Comment/Comment.tsx
+++ src/entities/postModal/Comment/Comment.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'next-i18next'
 
 import styles from './Comment.module.scss'
 
+import { useMeQuery } from '@/shared/api'
 import { PostResponseType } from '@/shared/api/services/posts/posts.api.types'
 import noImage from '@/shared/assets/icons/avatarProfile/notPhoto.png'
 import likeIcon from '@/shared/assets/icons/icons/likeIcon.svg'
@@ -18,52 +19,74 @@ type Props = {
 }
 
 export const Comment = ({ postData }: Props) => {
+  const {
+    owner: { lastName, firstName },
+    avatarOwner,
+    images,
+    updatedAt,
+    createdAt,
+    description,
+  } = postData
+
   const { t } = useTranslation('common', { keyPrefix: 'Post' })
-  const postCreatedAt = findDate.format(postData.createdAt)
-  const postUpdatedAt = findDate.difference(postData.updatedAt)
+  const postCreatedAt = findDate.format(createdAt)
+  const postUpdatedAt = findDate.difference(updatedAt)
+
+  const { data: me } = useMeQuery()
 
   return (
-    <>
-      <div className={styles.commentContainerWrapper}>
+    <div className={styles.commentContainerWrapper}>
+      <div className={styles.commentContainerInner}>
         <div className={styles.commentContainer}>
           <div className={styles.avatarContainer}>
-            <Image
-              src={postData.images[0].url ?? noImage}
-              alt={'avatar'}
-              layout="fill"
-              objectFit="cover"
-            />
+            <Image src={avatarOwner || noImage} alt={'avatar'} layout="fill" objectFit="cover" />
           </div>
           <div className={styles.commentTextAndLikeWrapper}>
             <div className={styles.commentTextContainer}>
               <p className={styles.commentText}>
-                <strong>{postData.owner.firstName || 'null'} </strong>
-                {postData?.description}
+                <strong>
+                  {firstName} {lastName}
+                </strong>{' '}
+                {description}
               </p>
-              <div className={styles.commentLikeContainer}>
-                <Link href={'#'}>
-                  <Image src={likeIcon} alt={''} />
-                </Link>
-              </div>
+              {me && (
+                <div className={styles.commentLikeContainer}>
+                  <Link href={'#'}>
+                    <Image src={likeIcon} alt={''} />
+                  </Link>
+                </div>
+              )}
             </div>
             <div className={styles.commentInfoContainer}>
               <p className={styles.commentTime}>{postUpdatedAt}</p>
-              <p className={styles.commentLikes}>{t('Likes')}: 12</p>
-              <p className={styles.commentAnswer}>{t('Answer')}</p>
+              {me && (
+                <>
+                  <p className={styles.commentLikes}>{t('Likes')}: 12</p>
+                  <p className={styles.commentAnswer}>{t('Answer')}</p>
+                </>
+              )}
             </div>
           </div>
         </div>
       </div>
       <div className={styles.summaryContainer}>
-        <div className={styles.actionsContainer}>
-          <div className={styles.likeShareContainer}>
-            <Image src={likeIcon} width={24} height={24} style={{ cursor: 'pointer' }} alt={''} />
-            <Image src={shareIcon} width={24} height={24} style={{ cursor: 'pointer' }} alt={''} />
+        {me && (
+          <div className={styles.actionsContainer}>
+            <div className={styles.likeShareContainer}>
+              <Image src={likeIcon} width={24} height={24} style={{ cursor: 'pointer' }} alt={''} />
+              <Image
+                src={shareIcon}
+                width={24}
+                height={24}
+                style={{ cursor: 'pointer' }}
+                alt={''}
+              />
+            </div>
+            <div>
+              <Image src={saveIcon} width={24} height={24} style={{ cursor: 'pointer' }} alt={''} />
+            </div>
           </div>
-          <div>
-            <Image src={saveIcon} width={24} height={24} style={{ cursor: 'pointer' }} alt={''} />
-          </div>
-        </div>
+        )}
         <div className={styles.totalLikes}>
           <div className={styles.avatarContainer}>
             <Image src={noImage} alt={'avatar'} width={36} height={36} />
@@ -72,12 +95,14 @@ export const Comment = ({ postData }: Props) => {
         </div>
         <div className={styles.postDate}>{postCreatedAt}</div>
       </div>
-      <div className={styles.addCommentContainer}>
-        <form className={styles.addCommentForm}>
-          <input className={styles.addCommentInput} placeholder={t('AddComment')} type="text" />
-          <button className={styles.addCommentButton}>{t('Publish')}</button>
-        </form>
-      </div>
-    </>
+      {me && (
+        <div className={styles.addCommentContainer}>
+          <form className={styles.addCommentForm}>
+            <input className={styles.addCommentInput} placeholder={t('AddComment')} type="text" />
+            <button className={styles.addCommentButton}>{t('Publish')}</button>
+          </form>
+        </div>
+      )}
+    </div>
   )
 }
