e88d0c69cfb00298c1b6a539a1712fa519bd3c45
Aliaksei <talerqa@gmail.com>
2024-01-13 18:24:33 +0300

    feat: add ui text component, fix: color and svg

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,34 +1,31 @@
-import type { StorybookConfig } from '@storybook/nextjs';
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+import type { StorybookConfig } from '@storybook/nextjs'
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
-  ],
-  "framework": {
-    "name": "@storybook/nextjs",
-    "options": {}
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
+  docs: {
+    autodocs: 'tag',
   },
-  webpackFinal: async (config) => {
+  webpackFinal: async config => {
     if (config.resolve) {
       config.resolve.plugins = [
         ...(config.resolve.plugins || []),
         new TsconfigPathsPlugin({
           extensions: config.resolve.extensions,
         }),
-      ];
+      ]
     }
-    return config;
-  }
-};
-export default config;
+    return config
+  },
+}
+export default config
